197872021385711a690a43e0329c298fd41d7c64
csharp404 <yousefjobsdeve@gmail.com>
2024-05-13 18:14:47 +0300

    Add files via upload

---
+++ multiplication.cs
@@ -0,0 +1,19 @@
+﻿// Yousef
+// [05/11/2024]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number :");
+        int num = -1;
+        if(int.TryParse(Console.ReadLine(),out int n))
+        {
+            num = n;
+        }
+        Console.WriteLine($"Multiplication table for {num} :");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{num} * {i} = {num*i}");
+        }
+    }
+}
